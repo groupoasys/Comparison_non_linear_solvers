22bccb9261f036d7546f47a1bf1d70ca631caaf6
asuncionjc <asuncionjc@uma.es>
2019-07-04 11:21:39 +0200

    1) Add the solver list of AMPL once AMPL has been installed. 2) Add the results for all the solver lists in a toy test, including the solvers in the ampl list

--- M2SVM_Optimal_Weights/main_m2svm_optimal_weights.py
+++ M2SVM_Optimal_Weights/main_m2svm_optimal_weights.py
@@ -50,6 +50,10 @@ csv_file_summary_results = 'summary_results'
 #                'filmint',
 #                'sbb',
 #                'scip']
+solvers_list_ampl = ['conopt',
+                    'loqo',
+                    'minos',
+                    'snopt']
 solvers_list_neos_flag_false = ['ipopt',
                                 'bonmin',
                                 'couenne']
@@ -67,7 +71,21 @@ solvers_list_neos_flag_true = ['conopt',
 cu.create_folder_results_if_it_doesnt_exits(folder_results = folder_results)
 cu.initialize_summary_results_file(folder_results = folder_results,
                                     csv_file_summary_results = csv_file_summary_results)
+for solver in solvers_list_ampl:
+    print(solver)
+    neos_flag = False
+    cu.run_optimization_problem_given_solver(solver = solver,
+                                             problem = problem,
+                                             neos_flag = neos_flag,
+                                             number_of_variables = number_of_variables,
+                                             number_of_constraints = number_of_constraints,
+                                             sense_opt_problem = sense_opt_problem,
+                                             maximum_number_iterations_multistart = maximum_number_iterations_multistart,
+                                             folder_results = folder_results,
+                                             csv_file_name_multistart = csv_file_name_multistart)
+
 for solver in solvers_list_neos_flag_false:
+    print(solver)
     neos_flag = False
     cu.run_optimization_problem_given_solver(solver = solver,
                                              problem = problem,
