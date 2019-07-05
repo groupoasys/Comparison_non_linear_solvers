56b33c9b6f6563d1118072891495ff2ec5bd4d32
asuncionjc <asuncionjc@uma.es>
2019-07-05 17:04:52 +0200

    The resolution of the multistart approach for a given solver has been simplified

--- Inverse_Optimization_Related_Problem/optimization_problem_utils/my_project/my_model.py
+++ Inverse_Optimization_Related_Problem/optimization_problem_utils/my_project/my_model.py
@@ -1,9 +1,15 @@
 import logging
 
+import Inverse_Optimization_Related_Problem.optimization_problem_utils.my_project.helpers as hp 
 import pyomo.environ as pe
 from pyomo.opt import TerminationCondition
 from pyomo.opt.base import SolverFactory
 import pdb
+import pandas as pd
+import time
+import numpy as np
+import os
+
 
  ####################################################
 # Model 1
@@ -134,3 +140,98 @@ def run_solver(instance,
         instance.solutions.load_from(results)
         instance.obj_model1.expr()
     return instance, results.solver, results.solution
+
+def run_mymodel(config,
+                neos_flag,
+                iteration_multistart,
+                results_dir='./results/',
+                solver='cplex'):
+    """ Run my model
+    
+    Parameters:
+        config (str): configuration file
+        results_dir (str): directory to store results
+        solver (str): solver to use (CPLEX, glpk, etc.)
+
+    """
+    results_dir = os.path.join(results_dir,config['output_files']['folder'])
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    
+    out_keys = ['info', 'theta', 'u', 'x', 'y']
+
+    dict_out = {}
+    for i in range(len(out_keys)):
+        dict_out[out_keys[i]] = pd.DataFrame([]) 
+
+    if config['model_cfg']['how_to_read_data']=='endogenously':
+        logging.info("## Creating input data endogenously ##")
+        data = {}
+        data['parameters'] = pd.DataFrame([], index=['theta', 'n'], columns=['Value'])
+        data['parameters'].loc['theta', 'Value'] = config['model_cfg']['theta'] 
+        data['parameters'].loc['n', 'Value'] = config['model_cfg']['n_sample']
+        data['parameters'].index.name = 'p'
+    
+        logging.info("## Generating input data for the parameter u ##")
+        np.random.seed(seed = 1133)
+        data['u'] = pd.DataFrame(np.random.uniform(-2,1,data['parameters'].loc['n', 'Value']), index=range(1, int(data['parameters'].loc['n', 'Value'])+1), columns=['u'])
+        data['u'].index.name = 'i'
+        
+    elif config['model_cfg']['how_to_read_data']=='exogenously':
+        # Reading the data
+        #logging.info("## Reading input data exogenously ##")
+        data = hp.parse_excel(config['input_files']['data'], config) 
+             
+    #logging.info("## Solving model 1 to generate x ##")
+    
+    
+    #Initialize variables multistart
+    
+    initial_variables_multistart = get_initial_variables_multistart(iteration_multistart = iteration_multistart,
+                                                                    config = config)
+    
+    # Create instance model
+    instance_mymodel = model1(data = data,
+                                 conf = config,
+                                 initial_variables_multistart = initial_variables_multistart)
+    
+    start_time = time.time()
+    solved_instance, solver_status, solver_solutions  = run_solver(instance_mymodel,
+                                                                      config['solver_cfg'],
+                                                                      solver = solver,
+                                                                      neos_flag = neos_flag)
+    end_time = time.time()
+        
+    print("--- %s seconds ---" % (time.time() - start_time))
+      
+    if solver_status.termination_condition.index == 8:  # Termination condition of solver: Optimal
+        dict_out['theta'].loc['theta', 'Value'] = data['parameters'].loc['theta', 'Value']
+        dict_out['x'].loc[:, 'x'] = hp.pyomo_to_pandas(solved_instance, 'x').iloc[:,0]
+        dict_out['y'].loc[:, 'y'] = hp.pyomo_to_pandas(solved_instance, 'y').iloc[:,0]
+        dict_out['u'].loc[:, 'u'] = data['u'].iloc[:, 0]
+        dict_out['info'].loc['Time', 'Value'] = (end_time - start_time)
+    
+    
+    output = {}
+    output['elapsed_time'] = dict_out['info'].loc['Time', 'Value']
+    output['objective_value'] = solved_instance.obj_model1()
+    
+    # Uncomment when the output should be saved in an output file
+    #mp.dict_pandas_to_excel(dict_out, dir=results_dir, filename=config['output_files']['filename'])
+    
+    return output
+def get_initial_variables_multistart(iteration_multistart,
+                                     config):
+    number_of_variables = config['model_cfg']['n_sample']
+    np.random.seed(seed = 1559 + iteration_multistart)
+    initial_variables_x = np.random.uniform(low = -10,
+                                            high = 10,
+                                            size = number_of_variables)
+    initial_variables_y = np.random.randint(low = 0,
+                                            high = 1 + 1,
+                                            size = number_of_variables)
+    initial_variables = pd.DataFrame({'x':initial_variables_x,
+                                      'y': initial_variables_y},
+                                     index = range(1, number_of_variables + 1))
+    
+    return initial_variables
